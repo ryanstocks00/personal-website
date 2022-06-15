46e63ca2c4b5f22451de4e5bc1c0e49d0e4c5cbf
Ryan Stocks <ryan.stocks00@gmail.com>
2022-06-16 00:38:46 +1000

    Some improvements

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import ReactGA from 'react-ga';
-import $ from 'jquery';
+import $, {jQuery} from 'jquery';
 import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
@@ -10,6 +9,7 @@ import Contact from './Components/Contact';
 import Testimonials from './Components/Testimonials';
 import Portfolio from './Components/Portfolio';
 
+
 class App extends Component {
 
   constructor(props){
@@ -18,9 +18,6 @@ class App extends Component {
       foo: 'bar',
       resumeData: {}
     };
-
-    ReactGA.initialize('UA-110570651-1');
-    ReactGA.pageview(window.location.pathname);
 
   }
 
@@ -41,6 +38,172 @@ class App extends Component {
 
   componentDidMount(){
     this.getResumeData();
+
+    //setTimeout(function() {
+	   //$('h1.responsive-headline').fitText(1, { minFontSize: '40px', maxFontSize: '90px' });
+	 //}, 100);
+
+
+/*----------------------------------------------------*/
+/* Smooth Scrolling
+------------------------------------------------------ */
+
+   $('.smoothscroll').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 800, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+
+
+/*----------------------------------------------------*/
+/* Highlight the current section in the navigation bar
+------------------------------------------------------*/
+
+    //var sections = $("section");
+    //var navigation_links = $("#nav-wrap a");
+
+    //sections.waypoint({
+
+      //handler: function(event, direction) {
+
+           //var active_section;
+
+            //active_section = $(this);
+            //if (direction === "up") active_section = active_section.prev();
+
+            //var active_link = $('#nav-wrap a[href="#' + active_section.attr("id") + '"]');
+
+         //navigation_links.parent().removeClass("current");
+            //active_link.parent().addClass("current");
+
+        //},
+        //offset: '35%'
+
+    //});
+
+
+/*----------------------------------------------------*/
+/*	Make sure that #header-background-image height is
+/* equal to the browser height.
+------------------------------------------------------ */
+
+   $('header').css({ 'height': $(window).height() });
+   $(window).on('resize', function() {
+
+        $('header').css({ 'height': $(window).height() });
+        $('body').css({ 'width': $(window).width() })
+   });
+
+
+/*----------------------------------------------------*/
+/*	Fade In/Out Primary Navigation
+------------------------------------------------------*/
+
+   $(window).on('scroll', function() {
+
+		var h = $('header').height();
+		var y = $(window).scrollTop();
+      var nav = $('#nav-wrap');
+
+	   if ( (y > h*.20) && (y < h) && ($(window).outerWidth() > 768 ) ) {
+	      nav.fadeOut('fast');
+	   }
+      else {
+         if (y < h*.20) {
+            nav.removeClass('opaque').fadeIn('fast');
+         }
+         else {
+            nav.addClass('opaque').fadeIn('fast');
+         }
+      }
+
+	});
+
+
+/*----------------------------------------------------*/
+/*	Modal Popup
+------------------------------------------------------*/
+
+    //$('.item-wrap a').magnificPopup({
+
+       //type:'inline',
+       //fixedContentPos: false,
+       //removalDelay: 200,
+       //showCloseBtn: false,
+       //mainClass: 'mfp-fade'
+
+    //});
+
+    //$(document).on('click', '.popup-modal-dismiss', function (e) {
+            //e.preventDefault();
+            //$.magnificPopup.close();
+    //});
+
+
+/*----------------------------------------------------*/
+/*	Flexslider
+/*----------------------------------------------------*/
+   //$('.flexslider').flexslider({
+      //namespace: "flex-",
+      //controlsContainer: ".flex-container",
+      //animation: 'slide',
+      //controlNav: true,
+      //directionNav: false,
+      //smoothHeight: true,
+      //slideshowSpeed: 7000,
+      //animationSpeed: 600,
+      //randomize: false,
+   //});
+
+/*----------------------------------------------------*/
+/*	contact form
+------------------------------------------------------*/
+
+   $('form#contactForm button.submit').click(function() {
+
+      $('#image-loader').fadeIn();
+
+      var contactName = $('#contactForm #contactName').val();
+      var contactEmail = $('#contactForm #contactEmail').val();
+      var contactSubject = $('#contactForm #contactSubject').val();
+      var contactMessage = $('#contactForm #contactMessage').val();
+
+      var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
+               '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
+
+      $.ajax({
+
+	      type: "POST",
+	      url: "inc/sendEmail.php",
+	      data: data,
+	      success: function(msg) {
+
+            // Message was sent
+            if (msg == 'OK') {
+               $('#image-loader').fadeOut();
+               $('#message-warning').hide();
+               $('#contactForm').fadeOut();
+               $('#message-success').fadeIn();   
+            }
+            // There was an error
+            else {
+               $('#image-loader').fadeOut();
+               $('#message-warning').html(msg);
+	            $('#message-warning').fadeIn();
+            }
+
+	      }
+
+      });
+      return false;
+   });
   }
 
   render() {
